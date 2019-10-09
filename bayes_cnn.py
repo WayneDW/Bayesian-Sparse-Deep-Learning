1738b9b81bed91e3d4b865f0944e5107f6cd4671
waynedw <greyman@live.cn>
2019-10-09 13:53:04 -0400

    .

--- bayes_cnn.py
+++ bayes_cnn.py
@@ -105,7 +105,7 @@ def main():
     
     """ Step 3: Load the model """
     if pars.prune > 0:
-        net = save_or_pretrain(net, 0, './output/pars.' + pars.data + '_' + pars.model)
+        net = save_or_pretrain(net, 0, './output/pars.' + pars.data + '_' + pars.model + '_seed_' + str(pars.seed))
         model_eval(net, test_loader, pars, 'Pretrained')
     else:
         print('Sampling from scratch')
@@ -113,7 +113,7 @@ def main():
     """ Step 4: Bayesian Sampling """
     sgmcmc(net, train_loader, test_loader, pars)
     if pars.save:
-        net = save_or_pretrain(net, 1, './output/pars.' + pars.data + '_' + pars.model)
+        net = save_or_pretrain(net, 1, './output/pars.' + pars.data + '_' + pars.model + '_seed_' + str(pars.seed))
 
 if __name__ == "__main__":
     main()
