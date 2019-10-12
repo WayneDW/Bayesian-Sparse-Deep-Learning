8c281c34fbc7684e12601bdf3f2fc4a8e9c316c1
waynedw <deng106@purdue.edu>
2019-10-12 11:48:09 -0400

    .

--- grid_search.py
+++ grid_search.py
@@ -16,17 +16,17 @@ else:
 for _ in range(1):
     #seed = str(random.randint(1, 10**6))
     seed = secure_random.choice(['125894', '130331', '258855', '421348', '248702', '248702', '248702'])
-    v0 = secure_random.choice(['0.01'])
-    v1 = secure_random.choice(['1e-4'])
+    v0 = secure_random.choice(['0.005'])
+    v1 = secure_random.choice(['1e-5'])
     sd = secure_random.choice(['1'])
-    lr = secure_random.choice(['2e-9'])
-    invT = secure_random.choice(['2e3', '1e4', '2e4'])
+    lr = secure_random.choice(['4e-9'])
+    dc = secure_random.choice(['0.1', '0.02'])
+    invT = secure_random.choice(['4e3', '2e4', '4e4'])
     anneal = secure_random.choice(['1.005', '1.007', '1.01'])
-    NN = secure_random.choice(['50000'])
+    NN = secure_random.choice(['25000'])
     sn = '1000'
     sparse = '0.90'
-    a, b = '9e5', '1e5'
     cut = secure_random.choice(['0.99'])
     #os.system('python bayes_cnn.py -prune 0 -save 1 -lr 0.1 -N ' + NN + ' -sn ' + sn + ' -v0 ' + v0 + ' -v1 ' + v1 + ' -seed ' + seed + ' -sparse 0 ' + ' -sd ' + sd + ' -invT 1e9 -gpu ' + gpu + ' -anneal 1 ' + ' > ./output/resnet20_cifar10_sn_' + sn + '_N_' + NN + '_v0_' + v0 + '_v1_' + v1 + '_sd_' + sd + '_invT_' + invT + '_anneal_1_pretrain_rand_' + seed)
 
-    os.system('python bayes_cnn.py -a ' + a + ' -b ' + b + ' -lr ' + lr + ' -prune 1 -N ' + NN + ' -sn ' + sn + ' -v0 ' + v0 + ' -v1 ' + v1 + ' -seed ' + seed + ' -sparse ' + sparse + ' -sd ' + sd + ' -invT ' + invT + ' -gpu ' + gpu + ' -anneal ' + anneal + ' > ./output/resnet20_cifar10_lr_' + lr + '_sn_' + sn + '_N_' + NN + '_a_' + a + '_b_' + b + '_v0_' + v0 + '_v1_' + v1 + '_sd_' + sd + '_invT_' + invT + '_anneal_' + anneal + '_sparse_' + sparse + '_rand_' + seed)
+    os.system('python bayes_cnn.py -dc ' + dc + ' -lr ' + lr + ' -prune 1 -N ' + NN + ' -sn ' + sn + ' -v0 ' + v0 + ' -v1 ' + v1 + ' -seed ' + seed + ' -sparse ' + sparse + ' -sd ' + sd + ' -invT ' + invT + ' -gpu ' + gpu + ' -anneal ' + anneal + ' > ./output/resnet20_cifar10_lr_' + lr + '_sn_' + sn + '_N_' + NN + '_dc_' + dc + '_v0_' + v0 + '_v1_' + v1 + '_sd_' + sd + '_invT_' + invT + '_anneal_' + anneal + '_sparse_' + sparse + '_rand_' + seed)
