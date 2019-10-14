853055a3d79fba100a9f549d731dbaf9ca5b796b
waynedw <greyman@live.cn>
2019-10-13 22:36:01 -0400

    .

--- grid_search.py
+++ grid_search.py
@@ -19,7 +19,9 @@ for _ in range(1):
     dc = secure_random.choice(['0.02'])
     invT = secure_random.choice(['1e3'])
     anneal, c = secure_random.choice([('1.005', 'sa'), ('1.005', 'em'), ('1.005', 'sghmc'), ('1.0', 'sa')])
+    anneal, c = ('1.005', 'sghmc')
     v0, v1, sparse = secure_random.choice([('0.5', '1e-3', '0.3'), ('0.1', '5e-4', '0.5'), ('0.1', '5e-5', '0.7'), ('0.005', '1e-5', '0.9')])
+    v0, v1, sparse = ('0.005', '1e-5', '0.9')
     #os.system('python bayes_cnn.py -prune 0 -save 1 -lr 2e-6 ' + ' -seed ' + seed + ' -sparse 0 ' + ' -invT 1e9 -gpu ' + gpu + ' -anneal 1 ' + ' > ./output/resnet20_cifar10_invT_' + invT + '_anneal_1_pretrain_rand_' + seed)
 
     os.system('python bayes_cnn.py -c ' + c + ' -dc ' + dc + ' -prune 1 ' + ' -v0 ' + v0 + ' -v1 ' + v1 + ' -seed ' + seed + ' -sparse ' + sparse + ' -invT ' + invT + ' -gpu ' + gpu + ' -anneal ' + anneal + ' > ./output/resnet20_cifar10_dc_' + dc + '_v0_' + v0 + '_v1_' + v1 + '_invT_' + invT + '_anneal_' + anneal + '_sparse_' + sparse + '_' + c + '_rand_' + seed)
